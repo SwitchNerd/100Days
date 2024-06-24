71b9f4ab981fdf5ee0f8d10162191ac1a5a6c2e1
SwitchNerd <avikamediwa@gmail.com>
2024-06-25 04:19:58 +0530

    Added_food_class

--- day20 & 21/main.py
+++ day20 & 21/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 import time
+from food import Food
 
 screen = Screen()
 screen.setup(width=600,height=600)
@@ -9,6 +10,7 @@ screen.title('Snake game')
 screen.tracer(0)
 
 snake = Snake()
+fods = Food()
 screen.listen()
 screen.onkeypress(snake.north,'Up')
 screen.onkeypress(snake.west, 'Left')
@@ -18,8 +20,12 @@ screen.onkeypress(snake.east, 'Right')
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.1) 
   
     snake.move()
+    
+    #Detect collision with food class
+    if snake.segments[0].distance(fods) < 15:
+        fods.refresh()
 
 screen.exitonclick()
