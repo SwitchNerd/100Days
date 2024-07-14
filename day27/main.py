8992f3c510ab877769e0696f680d12362e7eae2a
SwitchNerd <avikamediwa@gmail.com>
2024-07-13 22:55:41 -0700

    start

---
+++ day27/main.py
@@ -0,0 +1,13 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title('Mile to kilometers Converter')
+window.minsize(500,300)
+
+#label
+label = tkinter.Label(text="Test")
+label.pack(side='bottom')
+
+
+
+window.mainloop()
